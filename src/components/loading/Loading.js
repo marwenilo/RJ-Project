e28b102022e47de22d100782e84cf3ead49de3df
Marwen Magri <marwen.magri@gomycode.co>
2020-03-22 16:04:19 +0100

    prep for loading HOC

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Loading.css"
+ function Loading() {
+    return (
+        <div>
+        <div className='demo'>
+        <div className='circle'>
+          <div className='inner'></div>
+        </div>
+        <div className='circle'>
+          <div className='inner'></div>
+        </div>
+        <div className='circle'>
+          <div className='inner'></div>
+        </div>
+        <div className='circle'>
+          <div className='inner'></div>
+        </div>
+        <div className='circle'>
+          <div className='inner'></div>
+        </div>
+      </div>
+      <a href='http://google.com'>RightJob.com</a>
+      
+        </div>
+    )
+}
+
+export default Loading
