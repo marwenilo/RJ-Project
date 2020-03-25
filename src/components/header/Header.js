b24842685fa4d74ad79379d10f8672d03fbf474f
Marwen Magri <marwen.magri@gomycode.co>
2020-03-25 17:03:05 +0100

    delete and get ready for new nav bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import HeaderWrapper from "./headerWrapper";
-import SubHeader from "./components/subHeader";
-import TopHeader from "./components/topHeader";
+// import SubHeader from "./components/subHeader";
+// import TopHeader from "./components/topHeader";
 
 const Header = () => {
   return (
     <HeaderWrapper>
-      <TopHeader />
-      <SubHeader />
+     {/* <TopHeader />
+     <SubHeader />*/}
+     <p>prep for new nav bar</p>
     </HeaderWrapper>
   );
 };
